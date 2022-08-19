867369a54895898d4cc15d9ce4ca8892d231cc6d
jeongsikchoi <jeongsik.choi@knu.ac.kr>
2022-08-19 17:47:14 +0900

    imporve stability for server side

--- Server.py
+++ Server.py
@@ -8,14 +8,15 @@ from Filewriter import Filewriter
 
 TYPE_CSI_CLIENT = 0
 TYPE_FILE_TRANSFER_CLIENT = 1
+TYPE_STATUS_CHECKER_CLIENT = 2
+
 KEY_CSI_CLIENT = 'clkj209cjE!4k2GV#w02'
 KEY_FILE_TRANSFER_CLIENT = '#N2nb023flkn3lkj%!df'
+KEY_STATUS_CHECK_CLIENT = 'b0D^GV#ADG213kjhb0#G'
 
 BUFFER_SIZE = 2048
 SLEEP_TIME = 0.1
 FILE_CLOSE_TIME = 3600              # close file when the sever is idle for this amount of time
-
-client_timeout = 10                 # close connection if client is inactive for this time[s]
 
 class Server:
     def __init__(self, host, port):
@@ -56,8 +57,8 @@ class Server:
         # Classify client type
         connection.send(str.encode('You are connected to %s:%d' % (self.host, self.port)))
         mac_addr = connection.recv(BUFFER_SIZE).decode('UTF-8')         # when connected, clients are supposed to send their mac addresses and keys depending on their purposes
-        connection.send(str.encode('Received'))
         received_key = connection.recv(BUFFER_SIZE).decode('UTF-8')
+        connection.send(str.encode('Received'))
 
         if received_key == KEY_CSI_CLIENT:
             if not self.flag_file_opened:                               # open a file to write CSI data
@@ -68,13 +69,19 @@ class Server:
                 self.flag_file_opened = True
 
             self.client_counter += 1
-            client = ConnectedClient(TYPE_CSI_CLIENT, mac_addr, self.client_counter, connection, addr, self.init_time, self.packet_queue, self.close_connection, None)
+            client = ConnectedClient(TYPE_CSI_CLIENT, mac_addr, self.client_counter, connection, addr, self.close_connection, init_time=self.init_time, packet_queue=self.packet_queue)
             self.client_list.append(client)
 
 
         elif received_key == KEY_FILE_TRANSFER_CLIENT:
             self.client_counter += 1
-            client = ConnectedClient(TYPE_FILE_TRANSFER_CLIENT, self.client_counter, connection, addr, self.init_time, self.packet_queue, self.close_connection, self.get_server_status)
+            client = ConnectedClient(TYPE_FILE_TRANSFER_CLIENT, mac_addr, self.client_counter, connection, addr, self.close_connection)
+            self.client_list.append(client)
+
+
+        elif received_key == KEY_STATUS_CHECK_CLIENT:
+            self.client_counter += 1
+            client = ConnectedClient(TYPE_STATUS_CHECKER_CLIENT, mac_addr, self.client_counter, connection, addr, self.close_connection, server_status=self.get_server_status)
             self.client_list.append(client)
 
         else:
@@ -113,11 +120,31 @@ class Server:
                 self.client_list.pop(i)
                 break
 
+
     def get_server_status(self):
-        return 'A'
+        n_csi_client = 0
+        n_status_check_client = 0
+        n_file_transfer_client = 0
+
+        csi_client_status = 'CSI client list:\n'
+        for client in self.client_list:
+            if client.type == TYPE_CSI_CLIENT:
+                n_csi_client += 1
+                csi_client_status += '%s: transferred %d packets\n' % (client.name, client.packet_counter)
+            elif client.type == TYPE_FILE_TRANSFER_CLIENT:
+                n_file_transfer_client += 1
+            elif client.type == TYPE_STATUS_CHECKER_CLIENT:
+                n_status_check_client += 1
+
+        status = '%d csi, %d file transfer, %d status checke clients\n' % (n_csi_client, n_file_transfer_client, n_status_check_client)
+        status += csi_client_status
+
+        return status
+            
 
 class ConnectedClient:
-    def __init__(self, client_type, mac_addr, num, connection, addr, init_time, packet_queue, close_callback, server_status):
+    def __init__(self, client_type, mac_addr, num, connection, addr, close_callback,
+                    init_time = None, packet_queue = None, server_status = None):
         self.type = client_type
         self.name = mac_addr
         self.num = num
@@ -135,16 +162,39 @@ class ConnectedClient:
 
     def message_exchange_thread(self):
         if self.type == TYPE_CSI_CLIENT:    # keep receiving CSI data from client and put them to the queue
+            print('Connected new CSI client %s' % self.name)
             while True:
                 pkt = self.connection.recv(BUFFER_SIZE)
                 if len(pkt) == 0:
-                    print('CSI client %s is disconnected' % self.name)
+                    print('Disconnected CSI client %s' % self.name)
                     break
 
                 self.packet_counter += 1
                 elapsed_time = time.time() - self.init_time
                 print('%.2fs: packet num: %d (%d bytes) from %s (%s)' % (elapsed_time, self.packet_counter, len(pkt), self.name, self.ip))
                 self.packet_queue.put((elapsed_time, self.name, pkt))
+
+        elif self.type == TYPE_FILE_TRANSFER_CLIENT:
+            print('Connected new client for file transfer %s' % self.name)
+        
+        elif self.type == TYPE_STATUS_CHECKER_CLIENT:
+            print('Connected new client for status check %s' % self.name)
+            while True:
+                msg = self.connection.recv(BUFFER_SIZE)
+                if len(msg) == 0:
+                    print('Disconnected status check client %s' % self.name)
+                    break
+                
+                cmd = msg.decode('UTF-8')   # get command
+
+                if cmd == 'CMD_STATUS':
+                    self.connection.send(str.encode(self.server_status_callback()))
+                
+                if cmd == 'CMD_TIME':
+                    self.connection.send(str.encode('%.6f' % time.time()))
+
+                else:
+                    self.connection.send(str.decode("Undefined command"))
 
         # close connection
         self.connection.close()
